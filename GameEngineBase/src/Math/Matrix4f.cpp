0556cf6c50ec18bf9062e955b53edcb73a02253a
Mark Diedericks <diedgaming@gmail.com>
2017-08-27 17:10:19 +1000

    Matrix Math Updates

--- GameEngineBase/src/Math/Matrix4f.cpp
+++ GameEngineBase/src/Math/Matrix4f.cpp
@@ -3,217 +3,216 @@
 
 namespace gebase { namespace math {
 
-	Matrix4f::Matrix4f(void) {
-		for (int i = 0; i < 16; i++)
-			m[i] = 0;
+	Matrix4f::Matrix4f(void) 
+	{
+		memset(m, 0, 4 * 4 * sizeof(float));
 	}
 
-	Matrix4f::Matrix4f(const float data[]) {
-		for (int i = 0; i < 16; i++)
-			m[i] = data[i];
+	Matrix4f::Matrix4f(float* m) 
+	{
+		memcpy(this->m, m, 4 * 4 * sizeof(float));
+	}
+
+	Matrix4f::Matrix4f(const float m[])
+	{
+		memcpy(this->m, m, 4 * 4 * sizeof(float));
+	}
+
+	Matrix4f::Matrix4f(float diagonal)
+	{
+		memset(this->m, 0, 4 * 4 * sizeof(float));
+		m[0 + 0 * 4] = diagonal;
+		m[1 + 1 * 4] = diagonal;
+		m[2 + 2 * 4] = diagonal;
+		m[3 + 3 * 4] = diagonal;
 	}
 
 	Matrix4f& Matrix4f::initIdentity() {
-		Matrix4f* mat = new Matrix4f();
-		float* m = mat->m;
-
-		m[0] = 1; m[4] = 0;	m[8] = 0;   m[12] = 0;
-		m[1] = 0; m[5] = 1;	m[9] = 0;   m[13] = 0;
-		m[2] = 0; m[6] = 0;	m[10] = 1;  m[14] = 0;
-		m[3] = 0; m[7] = 0;	m[11] = 0;  m[15] = 1;
-		return *mat;
+		return Matrix4f(1);
 	}
 
 	Matrix4f& Matrix4f::initDiagonal(const float& diagonal) {
-		Matrix4f* mat = new Matrix4f();
-		float* m = mat->m;
-
-		m[0] = diagonal; m[4] = 0;			m[8] = 0;		   m[12] = 0;
-		m[1] = 0;		 m[5] = diagonal;	m[9] = 0;		   m[13] = 0;
-		m[2] = 0;		 m[6] = 0;			m[10] = diagonal;  m[14] = 0;
-		m[3] = 0;		 m[7] = 0;			m[11] = 0;		   m[15] = diagonal;
-		return *mat;
+		return Matrix4f(diagonal);
 	}
 
-	Matrix4f& Matrix4f::initPerspective(const float& fov, const float& aspectRatio, const float& zNear, const float& zFar) {
-		Matrix4f* mat = new Matrix4f();
-		float* m = mat->m;
+	Matrix4f& Matrix4f::initPerspective(const float& fov, const float& aspectRatio, const float& near, const float& far) {
+		Matrix4f result(1.0f);
 
-		float tanHalfFOV = (float)::tan(fov / 2.0f);
-		float zRange = zNear - zFar;
+		float q = 1.0f / tan(Utils::toRadians(0.5f * fov));
+		float a = q / aspectRatio;
 
-		m[0] = 1.0f / (tanHalfFOV * aspectRatio); m[4] = 0;					m[8] = 0;							m[12] = 0;
-		m[1] = 0;								  m[5] = 1.0f / tanHalfFOV;	m[9] = 0;							m[13] = 0;
-		m[2] = 0;								  m[6] = 0;					m[10] = (-zNear - zFar) / zRange;	m[14] = 2 * zFar * zNear / zRange;
-		m[3] = 0;								  m[7] = 0;					m[11] = 1;							m[15] = 0;
+		float b = (near + far) / (near - far);
+		float c = (2.0f * near * far) / (near - far);
 
-		return *mat;
+		result.m[0 + 0 * 4] = a;
+		result.m[1 + 1 * 4] = q;
+		result.m[2 + 2 * 4] = b;
+		result.m[2 + 3 * 4] = -1.0f;
+		result.m[3 + 2 * 4] = c;
+
+		return result;
 	}
 
 	Matrix4f& Matrix4f::initOrthographic(const float& left, const float& right, const float& top, const float& bottom, const float& near, const float& far) {
-		Matrix4f* mat = new Matrix4f();
-		float* m = mat->m;
+		Matrix4f result(1.0f);
 
-		float width = right - left;
-		float height = top - bottom;
-		float depth = far - near;
+		result.m[0 + 0 * 4] = 2.0f / (right - left);
 
-		m[0] = 2 / width; m[4] = 0;			 m[8] = 0;			 m[12] = -(right + left) / width;
-		m[1] = 0;		  m[5] = 2 / height; m[9] = 0;			 m[13] = -(top + bottom) / height;
-		m[2] = 0;		  m[6] = 0;		 	 m[10] = -2 / depth; m[14] = -(far + near) / depth;
-		m[3] = 0;		  m[7] = 0;			 m[11] = 0;			 m[15] = 1;
+		result.m[1 + 1 * 4] = 2.0f / (top - bottom);
 
-		return *mat;
+		result.m[2 + 2 * 4] = 2.0f / (near - far);
+
+		result.m[3 + 0 * 4] = (left + right) / (left - right);
+		result.m[3 + 1 * 4] = (bottom + top) / (bottom - top);
+		result.m[3 + 2 * 4] = (far + near) / (far - near);
+
+		return result;
 	}
 
-	Matrix4f& Matrix4f::initTranslation(const float& m_x, const float& y, const float& z) {
-		Matrix4f* mat = new Matrix4f();
-		float* m = mat->m;
+	Matrix4f& Matrix4f::initTranslation(const float& x, const float& y, const float& z) {
+		Matrix4f result(1.0f);
 
-		m[0] = 1; m[4] = 0;	m[8] = 0;   m[12] = m_x;
-		m[1] = 0; m[5] = 1;	m[9] = 0;   m[13] = y;
-		m[2] = 0; m[6] = 0;	m[10] = 1;  m[14] = z;
-		m[3] = 0; m[7] = 0;	m[11] = 0;  m[15] = 1;
-		return *mat;
+		result.m[3 + 0 * 4] = x;
+		result.m[3 + 1 * 4] = y;
+		result.m[3 + 2 * 4] = z;
+
+		return result;
 	}
 
-	Matrix4f& Matrix4f::initRotation(const float& m_x, const float& y, const float& z) {
-		Matrix4f* mat = new Matrix4f();
-		float* m = mat->m;
+	Matrix4f& Matrix4f::initScale(const float& x, const float& y, const float& z) {
+		Matrix4f result(1.0f);
 
-		Matrix4f rm_x = Matrix4f();
-		Matrix4f ry = Matrix4f();
-		Matrix4f rz = Matrix4f();
+		result.m[0 + 0 * 4] = x;
+		result.m[1 + 1 * 4] = y;
+		result.m[2 + 2 * 4] = z;
 
-		float m_x_ = Utils::toRadians(m_x);
-		float y_ = Utils::toRadians(y);
-		float z_ = Utils::toRadians(z);
-
-		rz.m[0] = (float)::cos(z);		rz.m[4] = -(float)::sin(z);			rz.m[8] = 0;					rz.m[12] = 0;
-		rz.m[1] = (float)::sin(z);		rz.m[5] = (float)::cos(z);			rz.m[9] = 0;					rz.m[13] = 0;
-		rz.m[2] = 0;		     		rz.m[6] = 0;						rz.m[10] = 1;					rz.m[14] = 0;
-		rz.m[3] = 0;					rz.m[7] = 0;						rz.m[11] = 0;					rz.m[15] = 1;
-
-		rm_x.m[0] = 1;					rm_x.m[4] = 0;						rm_x.m[8] = 0;					rm_x.m[12] = 0;
-		rm_x.m[1] = 0;					rm_x.m[5] = (float)::cos(m_x);		rm_x.m[9] = -(float)::sin(m_x); rm_x.m[13] = 0;
-		rm_x.m[2] = 0;					rm_x.m[6] = (float)::sin(m_x);		rm_x.m[10] = (float)::cos(m_x); rm_x.m[14] = 0;
-		rm_x.m[3] = 0;					rm_x.m[7] = 0;						rm_x.m[11] = 0;					rm_x.m[15] = 1;
-
-		ry.m[0] = (float)::cos(y);		ry.m[4] = 0;						ry.m[8] = -(float)::sin(y);		ry.m[12] = 0;
-		ry.m[1] = 0;					ry.m[5] = 1;						ry.m[9] = 0;					ry.m[13] = 0;
-		ry.m[2] = (float)::sin(y);		ry.m[6] = 0;						ry.m[10] = (float)::cos(y);		ry.m[14] = 0;
-		ry.m[3] = 0;					ry.m[7] = 0;						ry.m[11] = 0;					ry.m[15] = 1;
-
-		Matrix4f res = rz * (ry * rm_x);
-		for (int i = 0; i < 16; i++)
-			m[i] = res.m[i];
-
-		return *mat;
-	}
-
-	Matrix4f& Matrix4f::initRotation(const Vector3f& forward, const Vector3f& up) {
-		Vector3f f = forward;
-		f.normalize();
-
-		Vector3f r = up;
-		r.normalize();
-		r = r.cross(f);
-
-		Vector3f u = f.cross(r);
-
-		return Matrix4f::initRotation(f, u, r);
-	}
-
-	Matrix4f& Matrix4f::initRotation(const Vector3f& forward, const Vector3f& up, const Vector3f& right) {
-		Matrix4f* mat = new Matrix4f();
-		float* m = mat->m;
-
-		Vector3f f = forward;
-		Vector3f r = right;
-		Vector3f u = up;
-
-		m[0] = r.x;	m[4] = r.y;	m[8] = r.z;	m[12] = 0;
-		m[1] = u.x;	m[5] = u.y;	m[9] = u.z;	m[13] = 0;
-		m[2] = f.x;	m[6] = f.y;	m[10] = f.z;	m[14] = 0;
-		m[3] = 0;	    	m[7] = 0;			m[11] = 0;			m[15] = 1;
-
-		return *mat;
-	}
-
-	Matrix4f& Matrix4f::initScale(const float& m_x, const float& y, const float& z) {
-		Matrix4f* mat = new Matrix4f();
-		float* m = mat->m;
-
-		m[0] = m_x; m[4] = 0;	m[8] = 0;   m[12] = 0;
-		m[1] = 0; m[5] = y;	m[9] = 0;   m[13] = 0;
-		m[2] = 0; m[6] = 0;	m[10] = z;  m[14] = 0;
-		m[3] = 0; m[7] = 0;	m[11] = 0;  m[15] = 1;
-		return *mat;
-	}
-
-	float Matrix4f::get(const int& row, const int& column) const {
-		return m[(((column - 1) * 4) + row) - 1];
-	}
-
-	void Matrix4f::set(const int& row, const int& column, const float& value) {
-		m[(((column - 1) * 4) + row) - 1] = value;
-	}
-
-	Vector3f Matrix4f::transform(const Vector3f& r) {
-		Vector3f res(m[0] * r.x + m[4] * r.y + m[8]  * r.z + m[12],
-					 m[1] * r.x + m[5] * r.y + m[9]  * r.z + m[13],
-					 m[2] * r.x + m[6] * r.y + m[10] * r.z + m[14]);
-		return res;
-	}
-
-	Matrix4f Matrix4f::scale(const float& scale) {
-		for (int i = 0; i < 16; i++)
-			m[i] *= scale;
-
-		return *this;
+		return result;
 	}
 
 	Matrix4f Matrix4f::inverse() const{
-		Matrix4f inverse = Matrix4f();
-		inverse.initIdentity();
+		Matrix4f* inverse = new Matrix4f();
+		inverse->initIdentity();
 
-		float m[16];
+		float temp[16];
 
-		for (int i = 0; i < 16; i++)
-			m[i] = this->m[i];
+		temp[0] = m[5] * m[10] * m[15] -
+			m[5] * m[11] * m[14] -
+			m[9] * m[6] * m[15] +
+			m[9] * m[7] * m[14] +
+			m[13] * m[6] * m[11] -
+			m[13] * m[7] * m[10];
 
-		m[0]  = m[9]  * m[14] * m[7] - m[13] * m[10] * m[7] + m[13] * m[6] * m[11] - m[5] * m[14] * m[11] - m[9] * m[6] * m[15] + m[5] * m[10] * m[15];
-		m[4]  = m[12] * m[10] * m[7] - m[8]  * m[14] * m[7] - m[12] * m[6] * m[11] + m[4] * m[14] * m[11] + m[8] * m[6] * m[15] - m[4] * m[10] * m[15];
-		m[8]  = m[8]  * m[13] * m[7] - m[12] * m[9]  * m[7] + m[12] * m[5] * m[11] - m[4] * m[13] * m[11] - m[8] * m[5] * m[15] + m[4] * m[9]  * m[15];
-		m[12] = m[12] * m[9]  * m[6] - m[8]  * m[13] * m[6] - m[12] * m[5] * m[10] + m[4] * m[13] * m[10] + m[8] * m[5] * m[14] - m[4] * m[9]  * m[14];
-		m[1]  = m[13] * m[10] * m[3] - m[9]  * m[14] * m[3] - m[13] * m[2] * m[11] + m[1] * m[14] * m[11] + m[9] * m[2] * m[15] - m[1] * m[10] * m[15];
-		m[5]  = m[8]  * m[14] * m[3] - m[12] * m[10] * m[3] + m[12] * m[2] * m[11] - m[0] * m[14] * m[11] - m[8] * m[2] * m[15] + m[0] * m[10] * m[15];
-		m[9]  = m[12] * m[9]  * m[3] - m[8]  * m[13] * m[3] - m[12] * m[1] * m[11] + m[0] * m[13] * m[11] + m[8] * m[1] * m[15] - m[0] * m[9]  * m[15];
-		m[13] = m[8]  * m[13] * m[2] - m[12] * m[9]  * m[2] + m[12] * m[1] * m[10] - m[0] * m[13] * m[10] - m[8] * m[1] * m[14] + m[0] * m[9]  * m[14];
-		m[2]  = m[5]  * m[14] * m[3] - m[13] * m[6]  * m[3] + m[13] * m[2] * m[7]  - m[1] * m[14] * m[7]  - m[5] * m[2] * m[15] + m[1] * m[6]  * m[15];
-		m[6]  = m[12] * m[6]  * m[3] - m[4]  * m[14] * m[3] - m[12] * m[2] * m[7]  + m[0] * m[14] * m[7]  + m[4] * m[2] * m[15] - m[0] * m[6]  * m[15];
-		m[10] = m[4]  * m[13] * m[3] - m[12] * m[5]  * m[3] + m[12] * m[1] * m[7]  - m[0] * m[13] * m[7]  - m[4] * m[1] * m[15] + m[0] * m[5]  * m[15];
-		m[14] = m[12] * m[5]  * m[2] - m[4]  * m[13] * m[2] - m[12] * m[1] * m[6]  + m[0] * m[13] * m[6]  + m[4] * m[1] * m[14] - m[0] * m[5]  * m[14];
-		m[3]  = m[9]  * m[6]  * m[3] - m[5]  * m[10] * m[3] - m[9]  * m[2] * m[7]  + m[1] * m[10] * m[7]  + m[5] * m[2] * m[11] - m[1] * m[6]  * m[11];
-		m[7]  = m[4]  * m[10] * m[3] - m[8]  * m[6]  * m[3] + m[8]  * m[2] * m[7]  - m[0] * m[10] * m[7]  - m[4] * m[2] * m[11] + m[0] * m[6]  * m[11];
-		m[11] = m[8]  * m[5]  * m[3] - m[4]  * m[9]  * m[3] - m[8]  * m[1] * m[7]  + m[0] * m[9]  * m[7]  + m[4] * m[1] * m[11] - m[0] * m[5]  * m[11];
-		m[15] = m[4]  * m[9]  * m[2] - m[8]  * m[5]  * m[2] + m[8]  * m[1] * m[6]  - m[0] * m[9]  * m[6]  - m[4] * m[1] * m[10] + m[0] * m[5]  * m[10];
+		temp[4] = -m[4] * m[10] * m[15] +
+			m[4] * m[11] * m[14] +
+			m[8] * m[6] * m[15] -
+			m[8] * m[7] * m[14] -
+			m[12] * m[6] * m[11] +
+			m[12] * m[7] * m[10];
 
-		for (int i = 0; i < 16; i++)
-			inverse.m[i] = m[i];
+		temp[8] = m[4] * m[9] * m[15] -
+			m[4] * m[11] * m[13] -
+			m[8] * m[5] * m[15] +
+			m[8] * m[7] * m[13] +
+			m[12] * m[5] * m[11] -
+			m[12] * m[7] * m[9];
 
-		inverse.scale(1.0f / (float)this->determinant());
+		temp[12] = -m[4] * m[9] * m[14] +
+			m[4] * m[10] * m[13] +
+			m[8] * m[5] * m[14] -
+			m[8] * m[6] * m[13] -
+			m[12] * m[5] * m[10] +
+			m[12] * m[6] * m[9];
 
-		return inverse;
-	}
+		temp[1] = -m[1] * m[10] * m[15] +
+			m[1] * m[11] * m[14] +
+			m[9] * m[2] * m[15] -
+			m[9] * m[3] * m[14] -
+			m[13] * m[2] * m[11] +
+			m[13] * m[3] * m[10];
 
-	double Matrix4f::determinant() const {
-		double value = m[12] * m[9] * m[6]  * m[3]  - m[8] * m[13] * m[6]  * m[3]  - m[12] * m[5] * m[10] * m[3]  + m[4] * m[13] * m[10] * m[3]  +
-					   m[8]  * m[5] * m[14] * m[3]  - m[4] * m[9]  * m[14] * m[3]  - m[12] * m[9] * m[2]  * m[7]  + m[8] * m[13] * m[2]  * m[7]  +
-					   m[12] * m[1] * m[10] * m[7]  - m[0] * m[13] * m[10] * m[7]  - m[8]  * m[1] * m[14] * m[7]  + m[0] * m[9]  * m[14] * m[7]  +
-					   m[12] * m[5] * m[2]  * m[11] - m[4] * m[13] * m[2]  * m[11] - m[12] * m[1] * m[6]  * m[11] + m[0] * m[13] * m[6]  * m[11] +
-					   m[4]  * m[1] * m[14] * m[11] - m[0] * m[5]  * m[14] * m[11] - m[8]  * m[5] * m[2]  * m[15] + m[4] * m[9]  * m[2]  * m[15] +
-					   m[8]  * m[1] * m[6]  * m[15] - m[0] * m[9]  * m[6]  * m[15] - m[4]  * m[1] * m[10] * m[15] + m[0] * m[5]  * m[10] * m[15] ;
-		return value;
+		temp[5] = m[0] * m[10] * m[15] -
+			m[0] * m[11] * m[14] -
+			m[8] * m[2] * m[15] +
+			m[8] * m[3] * m[14] +
+			m[12] * m[2] * m[11] -
+			m[12] * m[3] * m[10];
+
+		temp[9] = -m[0] * m[9] * m[15] +
+			m[0] * m[11] * m[13] +
+			m[8] * m[1] * m[15] -
+			m[8] * m[3] * m[13] -
+			m[12] * m[1] * m[11] +
+			m[12] * m[3] * m[9];
+
+		temp[13] = m[0] * m[9] * m[14] -
+			m[0] * m[10] * m[13] -
+			m[8] * m[1] * m[14] +
+			m[8] * m[2] * m[13] +
+			m[12] * m[1] * m[10] -
+			m[12] * m[2] * m[9];
+
+		temp[2] = m[1] * m[6] * m[15] -
+			m[1] * m[7] * m[14] -
+			m[5] * m[2] * m[15] +
+			m[5] * m[3] * m[14] +
+			m[13] * m[2] * m[7] -
+			m[13] * m[3] * m[6];
+
+		temp[6] = -m[0] * m[6] * m[15] +
+			m[0] * m[7] * m[14] +
+			m[4] * m[2] * m[15] -
+			m[4] * m[3] * m[14] -
+			m[12] * m[2] * m[7] +
+			m[12] * m[3] * m[6];
+
+		temp[10] = m[0] * m[5] * m[15] -
+			m[0] * m[7] * m[13] -
+			m[4] * m[1] * m[15] +
+			m[4] * m[3] * m[13] +
+			m[12] * m[1] * m[7] -
+			m[12] * m[3] * m[5];
+
+		temp[14] = -m[0] * m[5] * m[14] +
+			m[0] * m[6] * m[13] +
+			m[4] * m[1] * m[14] -
+			m[4] * m[2] * m[13] -
+			m[12] * m[1] * m[6] +
+			m[12] * m[2] * m[5];
+
+		temp[3] = -m[1] * m[6] * m[11] +
+			m[1] * m[7] * m[10] +
+			m[5] * m[2] * m[11] -
+			m[5] * m[3] * m[10] -
+			m[9] * m[2] * m[7] +
+			m[9] * m[3] * m[6];
+
+		temp[7] = m[0] * m[6] * m[11] -
+			m[0] * m[7] * m[10] -
+			m[4] * m[2] * m[11] +
+			m[4] * m[3] * m[10] +
+			m[8] * m[2] * m[7] -
+			m[8] * m[3] * m[6];
+
+		temp[11] = -m[0] * m[5] * m[11] +
+			m[0] * m[7] * m[9] +
+			m[4] * m[1] * m[11] -
+			m[4] * m[3] * m[9] -
+			m[8] * m[1] * m[7] +
+			m[8] * m[3] * m[5];
+
+		temp[15] = m[0] * m[5] * m[10] -
+			m[0] * m[6] * m[9] -
+			m[4] * m[1] * m[10] +
+			m[4] * m[2] * m[9] +
+			m[8] * m[1] * m[6] -
+			m[8] * m[2] * m[5];
+
+		float determinant = m[0] * temp[0] + m[1] * temp[4] + m[2] * temp[8] + m[3] * temp[12];
+		determinant = 1.0f / determinant;
+
+		for (int32 i = 0; i < 4 * 4; i++)
+			inverse->m[i] = temp[i] * determinant;
+
+		return *inverse;
 	}
 } }
--- GameEngineBase/src/Math/Matrix4f.h
+++ GameEngineBase/src/Math/Matrix4f.h
@@ -16,24 +16,18 @@ namespace gebase { namespace math {
 			Vector4f rows[4];
 		};
 	public:
-		Matrix4f(void);
-		Matrix4f(const float data[]);
+		Matrix4f();
+		Matrix4f(const float elements[]);
+		Matrix4f(float* elements);
+		Matrix4f(float diagonal);
 		static Matrix4f& initIdentity();
 		static Matrix4f& initDiagonal(const float& diagonal);
 		static Matrix4f& initPerspective(const float& fov, const float& aspectRatio, const float& zNear, const float& zFar);
 		static Matrix4f& initOrthographic(const float& left, const float& right, const float& top, const float& bottom, const float& near, const float& far);
 		static Matrix4f& initTranslation(const float& m_x, const float& y, const float& z);
 		static inline Matrix4f& initTranslation(const Vector3f& position) { return Matrix4f::initTranslation(position.x, position.y, position.z); }
-		static Matrix4f& initRotation(const float& m_x, const float& y, const float& z);
-		static Matrix4f& initRotation(const Vector3f& forward, const Vector3f& up);
-		static Matrix4f& initRotation(const Vector3f& forward, const Vector3f& up, const Vector3f& right);
 		static Matrix4f& initScale(const float& m_x, const float& y, const float& z);
-		float get(const int& row, const int& column) const;
-		void set(const int& row, const int& column, const float& value);
-		Vector3f transform(const Vector3f& vec3f);
-		Matrix4f scale(const float& scale);
 		Matrix4f inverse() const;
-		double determinant() const;
 
 		inline void operator =(const Matrix4f& mat4f) {
 			for (int i = 0; i < 16; i++)
@@ -63,9 +57,20 @@ namespace gebase { namespace math {
 		}
 
 		inline Matrix4f& operator *=(const Matrix4f& mat4f) {
-			for (int m_x = 0; m_x < 4; m_x++)
-				for (int y = 0; y < 4; y++)
-					this->set(m_x, y, get(m_x, 0) * (mat4f.get(0, y)) + get(m_x, 1) * (mat4f.get(1, y)) + get(m_x, 2) * (mat4f.get(2, y)) + get(m_x, 3) * (mat4f.get(3, y)));
+			float data[16];
+			for (int32 row = 0; row < 4; row++)
+			{
+				for (int32 col = 0; col < 4; col++)
+				{
+					float sum = 0.0f;
+					for (int32 e = 0; e < 4; e++)
+					{
+						sum += m[e + row * 4] * mat4f.m[col + e * 4];
+					}
+					data[col + row * 4] = sum;
+				}
+			}
+			memcpy(m, data, 4 * 4 * sizeof(float));
 			return *this;
 		}
 
