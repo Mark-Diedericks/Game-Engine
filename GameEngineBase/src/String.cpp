3e0e43dcef81be978b7a4c22ec7f0d814b4e9443
Mark Diedericks <diedgaming@gmail.com>
2017-08-27 13:31:16 +1000

    String class edits

--- GameEngineBase/src/String.cpp
+++ GameEngineBase/src/String.cpp
@@ -2,18 +2,19 @@
 #include "String.h"
 
 namespace gebase {
-	char* StringFormat::m_Buffer = new char[STRING_FORMAT_BUFFER_SIZE];
 
-	std::vector<String> SplitString(const String& str, const String& delimeters) 
-	{ 
+	char* StringFormat::s_Buffer = new char[STRINGFORMAT_BUFFER_SIZE];
+
+	std::vector<String> SplitString(const String& string, const String& delimiters)
+	{
 		size_t start = 0;
-		size_t end = str.find_first_of(delimeters);
+		size_t end = string.find_first_of(delimiters);
 
 		std::vector<String> result;
 
-		while (end <= String::npos) {
-			String token = str.substr(start, end - start);
-
+		while (end <= String::npos)
+		{
+			String token = string.substr(start, end - start);
 			if (!token.empty())
 				result.push_back(token);
 
@@ -21,138 +22,116 @@ namespace gebase {
 				break;
 
 			start = end + 1;
-			end = str.find_first_of(delimeters, start);
+			end = string.find_first_of(delimiters, start);
 		}
 
 		return result;
 	}
 
-	std::vector<String> SplitString(const String& str, const char delimeter) 
-	{ 
-		return SplitString(str, String(1, delimeter));
+	std::vector<String> SplitString(const String& string, const char delimiter)
+	{
+		return SplitString(string, String(1, delimiter));
 	}
 
-	std::vector<String> Tokenize(const String& str) 
-	{ 
-		return SplitString(str, " \t\n");
+	std::vector<String> Tokenize(const String& string)
+	{
+		return SplitString(string, " \t\n");
 	}
 
-	std::vector<String> GetLines(const String& str) 
-	{ 
-		return SplitString(str, "\n");
+	std::vector<String> GetLines(const String& string)
+	{
+		return SplitString(string, "\n");
 	}
 
-	const char* FindToken(const char* str, const String& token) 
-	{ 
+	const char* FindToken(const char* str, const String& token)
+	{
 		const char* t = str;
-
-		while (t = strstr(t, token.c_str())) {
+		while (t = strstr(t, token.c_str()))
+		{
 			bool left = str == t || isspace(t[-1]);
 			bool right = !t[token.size()] || isspace(t[token.size()]);
-
 			if (left && right)
 				return t;
 
 			t += token.size();
 		}
-
 		return nullptr;
 	}
 
-	const char* FindToken(const String& str, const String& token) 
-	{ 
-		return FindToken(str.c_str(), token);
+	const char* FindToken(const String& string, const String& token)
+	{
+		return FindToken(string.c_str(), token);
 	}
 
-	int32 IndexOf(const String& str, const String& search, uint offset) 
-	{ 
-		const char* cstr = str.c_str() + offset;
-		const char* found = strstr(cstr, search.c_str());
-
+	int32 FindStringPosition(const String& string, const String& search, uint offset)
+	{
+		const char* str = string.c_str() + offset;
+		const char* found = strstr(str, search.c_str());
 		if (found == nullptr)
 			return -1;
-
-		return (int32)(found - cstr) + offset;
+		return (int32)(found - str) + offset;
 	}
 
-	int32 IndexOf(const String& str, const char* search, uint offset)
+	String StringRange(const String& string, uint start, uint length)
 	{
-		return IndexOf(str, String(search), offset);
+		return string.substr(start, length);
 	}
 
-	String Substring(const String& str, uint start, uint length) 
-	{ 
-		return str.substr(start, length);
-	}
-
-	String Remove(const String& str, uint start, uint length) 
+	String RemoveStringRange(const String& string, uint start, uint length)
 	{
-		String result = str;
+		String result = string;
 		return result.erase(start, length);
 	}
 
-	String GetBlock(const char* str, const char** outPosition) 
-	{ 
-		const char* end = strstr(str, "}");
-
-		if (!end)
-			return String(str);
-
-		if (outPosition)
-			*outPosition = end;
-
-		uint length = end - str + 1;
-
-		return String(str, length);
-	}
-
-	String GetBlock(const String& str, uint offset) 
-	{ 
-		const char* cstr = str.c_str() + offset;
-		return GetBlock(cstr);
-	}
-
-	String GetStatement(const char* str, const char** outPosition) 
-	{ 
-		const char* end = strstr(str, ";");
-
-		if (!end)
-			return String(str);
-
-		if (outPosition)
-			*outPosition = end;
-
-		uint length = end - str + 1;
-
-		return String(str, length);
-	}
-
-	bool Contains(const String& str, const String& chars) 
-	{ 
-		return str.find(chars) != String::npos;
-	}
-
-	bool StartsWith(const String& str, const String& chars) 
-	{ 
-		return str.find(chars) == 0;
-	}
-
-	bool EndsWith(const String& str, const String& chars) 
-	{ 
-		String result = str;
-		std::reverse(result.begin(), result.end());
-		return result.find(chars) == 0;
-
-	}
-
-	int32 NextInt(const String& str)
+	String GetBlock(const char* str, const char** outPosition)
 	{
-		const char* cstr = str.c_str();
+		const char* end = strstr(str, "}");
+		if (!end)
+			return String(str);
 
-		for (uint i = 0; i < str.size(); i++)
-			if (isdigit(str[i]))
-				return atoi(&str[i]);
+		if (outPosition)
+			*outPosition = end;
+		uint length = end - str + 1;
+		return String(str, length);
+	}
 
+	String GetBlock(const String& string, uint offset)
+	{
+		const char* str = string.c_str() + offset;
+		return GetBlock(str);
+	}
+
+	String GetStatement(const char* str, const char** outPosition)
+	{
+		const char* end = strstr(str, ";");
+		if (!end)
+			return String(str);
+
+		if (outPosition)
+			*outPosition = end;
+		uint length = end - str + 1;
+		return String(str, length);
+	}
+
+	bool StringContains(const String& string, const String& chars)
+	{
+		return string.find(chars) != String::npos;
+	}
+
+	bool StartsWith(const String& string, const String& start)
+	{
+		return string.find(start) == 0;
+	}
+
+	int32 NextInt(const String& string)
+	{
+		const char* str = string.c_str();
+		for (uint i = 0; i < string.size(); i++)
+		{
+			if (isdigit(string[i]))
+				return atoi(&string[i]);
+		}
 		return -1;
 	}
+
 }
--- GameEngineBase/src/String.h
+++ GameEngineBase/src/String.h
@@ -8,65 +8,62 @@
 typedef std::string String;
 
 namespace gebase {
+#define STRINGFORMAT_BUFFER_SIZE 10 * 1024
 
-#define STRING_FORMAT_BUFFER_SIZE 10240
-
-	class GE_API StringFormat 
+	class GE_API StringFormat
 	{
 	private:
-		static char* m_Buffer;
+		static char* s_Buffer;
 	public:
 		template<typename T>
-		static String Hex(const T& input) {
-			memset(m_Buffer, 0, STRING_FORMAT_BUFFER_SIZE);
-			sprintf(m_Buffer, "%02x", input);
-			return String(m_Buffer);
+		static String Hex(const T& input)
+		{
+			memset(s_Buffer, 0, STRINGFORMAT_BUFFER_SIZE);
+			sprintf(s_Buffer, "%02x", input);
+			return String(s_Buffer);
 		}
 
 		template<typename T>
-		static String Hex(const T* input, uint size) {
-			memset(m_Buffer, 0, STRING_FORMAT_BUFFER_SIZE);
+		static String Hex(const T* input, uint size)
+		{
+			memset(s_Buffer, 0, STRINGFORMAT_BUFFER_SIZE);
 			for (uint i = 0; i < size; i++)
-				sprintf(m_Buffer + i * 3, "%02x", input[i]);
-			return String(m_Buffer);
+				sprintf(s_Buffer + i * 3, "%02x ", input[i]);
+			return String(s_Buffer);
 		}
 
-		static String Float(const float input, uint place = 2) {
-			memset(m_Buffer, 0, STRING_FORMAT_BUFFER_SIZE);
-			sprintf(m_Buffer, "%.2f", input);
-			return String(m_Buffer);
+		static String Float(const float input, uint places = 2)
+		{
+			memset(s_Buffer, 0, STRINGFORMAT_BUFFER_SIZE);
+			sprintf(s_Buffer, "%.2f", input);
+			return String(s_Buffer);
 		}
 
 		template<typename T>
-		static String ToString(const T& input) {
+		static String ToString(const T& input)
+		{
 			return std::to_string(input);
 		}
 	};
 
-	std::vector<String> SplitString(const String& str, const String& delimeters);
-	std::vector<String> SplitString(const String& str, const char delimeter);
-
-	std::vector<String> Tokenize(const String& str);
-	std::vector<String> GetLines(const String& str);
+	std::vector<String> SplitString(const String& string, const String& delimiters);
+	std::vector<String> SplitString(const String& string, const char delimiter);
+	std::vector<String> Tokenize(const String& string);
+	std::vector<String> GetLines(const String& string);
 
 	const char* FindToken(const char* str, const String& token);
-	const char* FindToken(const String& str, const String& token);
-
-	int32 IndexOf(const String& str, const String& search, uint offset = 0);
-	int32 IndexOf(const String& str, const char* search, uint offset = 0);
-
-	String Substring(const String& str, uint start, uint length);
-
-	String Remove(const String& str, uint start, uint length);
+	const char* FindToken(const String& string, const String& token);
+	int32 FindStringPosition(const String& string, const String& search, uint offset = 0);
+	String StringRange(const String& string, uint start, uint length);
+	String RemoveStringRange(const String& string, uint start, uint length);
 
 	String GetBlock(const char* str, const char** outPosition = nullptr);
-	String GetBlock(const String& str, uint offset = 0);
+	String GetBlock(const String& string, uint offset = 0);
 
 	String GetStatement(const char* str, const char** outPosition = nullptr);
 
-	bool Contains(const String& str, const String& chars);
-	bool StartsWith(const String& str, const String& chars);
-	bool EndsWith(const String& str, const String& chars);
+	bool StringContains(const String& string, const String& chars);
+	bool StartsWith(const String& string, const String& start);
+	int32 NextInt(const String& string);
 
-	int32 NextInt(const String& str);
 }
