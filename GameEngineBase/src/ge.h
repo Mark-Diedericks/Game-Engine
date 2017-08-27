cb685043a932f5fbf5fce20f03bd8a54d7ee176a
Mark Diedericks <diedgaming@gmail.com>
2017-08-27 12:09:51 +1000

    PCH editing

--- GameEngineBase/src/ge.h
+++ GameEngineBase/src/ge.h
@@ -2,16 +2,16 @@
 
 //PCH HEADER
 
-//#include <algorithm>		<- Doesn't compile
-//#include <functional>		<- Doesn't compile
-//#include <iostream>		<- Doesn't compile
-//#include <string>			<- Doesn't compile
-//#include <sstream>		<- Doesn't compile
+#include <algorithm>		
+#include <functional>		
+#include <iostream>		
+#include <string>			
+#include <sstream>		
 
-//#include <vector>			<- Doesn't compile
-//#include <list>			<- Doesn't compile
-//#include <map>			<- Doesn't compile
-//#include <unordered_map>	<- Doesn't compile
+#include <vector>			
+#include <list>			
+#include <map>			
+#include <unordered_map>	
 
 #include <cstddef>
 #include <stdio.h>
