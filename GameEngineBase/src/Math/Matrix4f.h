9183b670c05f2c6d5865d646083a3d475e88fca1
Mark Diedericks <diedgaming@gmail.com>
2017-09-01 20:42:53 +1000

    Matrix4f reverted

--- GameEngineBase/src/Math/Matrix4f.h
+++ GameEngineBase/src/Math/Matrix4f.h
@@ -3,71 +3,101 @@
 #include <math.h>
 
 #include "Common.h"
-#include "CustomString.h"
 #include "Vector3f.h"
 #include "Vector4f.h"
 #include "Utils.h"
 
-namespace gebase { namespace math {
+namespace gebase {
+	namespace math {
 
-	class Quaternion;
+		class Quaternion;
 
-	class GE_API Matrix4f 
-	{
-	public:
-		union
-		{
-			float elements[4 * 4];
-			Vector4f rows[4];
+		class GE_API Matrix4f {
+		public:
+			union
+			{
+				float m[4 * 4];
+				Vector4f rows[4];
+			};
+		public:
+			Matrix4f();
+			Matrix4f(const float elements[]);
+			Matrix4f(float* elements);
+			Matrix4f(float diagonal);
+			static Matrix4f Identity();
+			static Matrix4f Diagonal(const float& diagonal);
+			static Matrix4f Perspective(const float& fov, const float& aspectRatio, const float& zNear, const float& zFar);
+			static Matrix4f Orthographic(const float& left, const float& right, const float& bottom, const float& top, const float& near, const float& far);
+			static Matrix4f Translation(const float& x, const float& y, const float& z);
+			static inline Matrix4f Translation(const Vector3f& position) { return Matrix4f::Translation(position.x, position.y, position.z); }
+			static Matrix4f Rotation(const Quaternion& quat);
+			static Matrix4f Scale(const float& m_x, const float& y, const float& z);
+			Matrix4f inverse() const;
+
+			inline void operator =(const Matrix4f& mat4f) {
+				for (int i = 0; i < 16; i++)
+					m[i] = mat4f.m[i];
+			}
+
+			inline bool operator ==(const Matrix4f& mat4f) const {
+				for (int i = 0; i < 16; i++)
+					if (m[i] != mat4f.m[i])
+						return false;
+
+				return true;
+			}
+
+			inline Matrix4f& operator +=(const Matrix4f& mat4f) {
+				for (int i = 0; i < 16; i++)
+					m[i] += mat4f.m[i];
+
+				return *this;
+			}
+
+			inline Matrix4f& operator -=(const Matrix4f& mat4f) {
+				for (int i = 0; i < 16; i++)
+					m[i] -= mat4f.m[i];
+
+				return *this;
+			}
+
+			inline Matrix4f& operator *=(const Matrix4f& mat4f) {
+				float data[16];
+				for (int32 row = 0; row < 4; row++)
+				{
+					for (int32 col = 0; col < 4; col++)
+					{
+						float sum = 0.0f;
+						for (int32 e = 0; e < 4; e++)
+						{
+							sum += m[e + row * 4] * mat4f.m[col + e * 4];
+						}
+						data[col + row * 4] = sum;
+					}
+				}
+				memcpy(m, data, 4 * 4 * sizeof(float));
+				return *this;
+			}
+
+			inline Matrix4f operator +(const Matrix4f& mat4f) const {
+				return Matrix4f(m) += mat4f;
+			}
+
+			inline Matrix4f operator -(const Matrix4f& mat4f) const {
+				return Matrix4f(m) -= mat4f;
+			}
+
+			inline Matrix4f operator *(const Matrix4f& mat4f) const {
+				return Matrix4f(m) *= mat4f;
+			}
+
+			inline Vector3f operator *(const Vector3f& vec3f) const {
+				return vec3f.Mul(*this);
+			}
+
+			inline Vector4f operator *(const Vector4f& vec4f) const {
+				return vec4f.Mul(*this);
+			}
 		};
-
-		Matrix4f();
-		Matrix4f(float diagonal);
-		Matrix4f(float* elements);
-		Matrix4f(const Vector4f& row0, const Vector4f& row1, const Vector4f& row2, const Vector4f& row3);
-
-		static Matrix4f Identity();
-
-		Matrix4f& Multiply(const Matrix4f& other);
-		friend GE_API Matrix4f operator*(Matrix4f left, const Matrix4f& right);
-		Matrix4f& operator*=(const Matrix4f& other);
-
-		Vector3f Multiply(const Vector3f& other) const;
-		friend GE_API Vector3f operator*(const Matrix4f& left, const Vector3f& right);
-
-		Vector4f Multiply(const Vector4f& other) const;
-		friend GE_API Vector4f operator*(const Matrix4f& left, const Vector4f& right);
-
-		void operator =(const Matrix4f& mat4f);
-
-		Matrix4f& Invert();
-
-		Vector4f GetColumn(int index) const;
-		void SetColumn(uint index, const Vector4f& column);
-		inline Vector3f GetPosition() const { return Vector3f(GetColumn(3)); }
-		inline void SetPosition(const Vector3f& position) { SetColumn(3, Vector4f(position, 1.0f)); }
-
-		static Matrix4f Orthographic(float left, float right, float bottom, float top, float near, float far);
-		static Matrix4f Perspective(float fov, float aspectRatio, float near, float far);
-		static Matrix4f LookAt(const Vector3f& camera, const Vector3f& object, const Vector3f& up);
-
-		static Matrix4f Translate(const Vector3f& translation);
-		static inline Matrix4f Translation(const Vector3f& translation) { return Translate(translation); }
-		static inline Matrix4f Translation(const float& x, const float& y, const float& z) { return Translate(Vector3f(x, y, z)); }
-
-		static Matrix4f Rotate(float angle, const Vector3f& axis);
-		static Matrix4f Rotation(const Vector3f& axis, float angle) { return Rotate(angle, axis); }
-
-		static Matrix4f Rotate(const Quaternion& quat);
-		static inline Matrix4f Rotation(const Quaternion& quat) { return Rotate(quat); }
-
-		static Matrix4f Scale(const Vector3f& scale);
-		static inline Matrix4f Scale(const float& x, const float& y, const float& z) { return Scale(Vector3f(x, y, z)); }
-
-		static Matrix4f Invert(const Matrix4f& matrix);
-
-		static Matrix4f Transpose(const Matrix4f& matrix);
-
-		String ToString() const;
-	};
-} }
+	}
+}
